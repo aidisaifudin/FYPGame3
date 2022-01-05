27a6d02ab7ccffa96b4a33cde249d3c305e4b7ff
Toastyship <64509653+Toastyship@users.noreply.github.com>
2022-01-05 16:20:44 +0800

    A
    A

---
+++ XML File/Assets/Scripts/ParseXML.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.IO;
+using TMPro;
+
+public class ParseXML : MonoBehaviour {
+	public TextAsset scoreDataTextFile;
+	public TextMeshProUGUI textDisplay;
+
+	void Start() {
+		string textData = scoreDataTextFile.text;
+		ParseScoreXML(textData);
+	}
+	
+	void ParseScoreXML(string xmlData) {
+		XmlDocument xmlDoc = new XmlDocument();
+		xmlDoc.Load(new StringReader(xmlData));
+		
+		string xmlPathPattern = "//scoreRecordList/scoreRecord";
+		XmlNodeList myNodeList = xmlDoc.SelectNodes(xmlPathPattern);
+
+		foreach (XmlNode node in myNodeList)
+			textDisplay.text += ScoreRecordString(node) + "<br>";
+			//Debug.Log(ScoreRecordString(node));
+
+		XmlNodeList item = xmlDoc.GetElementsByTagName("player");
+		textDisplay.text += item[0].Name + ": " + item[0].ChildNodes[0].InnerText;
+	}
+	
+	string ScoreRecordString(XmlNode node) {
+		XmlNode playerNode = node.FirstChild;
+		XmlNode scoreNode = playerNode.NextSibling;
+		XmlNode dateNode = scoreNode.NextSibling;
+		
+		return "Player = " + playerNode.InnerXml + ", score = " + scoreNode.InnerXml + ", date = " + DateString(dateNode);
+	}
+	
+	string DateString(XmlNode dateNode) {
+		XmlNode dayNode = dateNode.FirstChild;
+		XmlNode monthNode = dayNode.NextSibling;
+		XmlNode yearNode = monthNode.NextSibling;
+		
+		return dayNode.InnerXml + "/" + monthNode.InnerXml + "/" + yearNode.InnerXml;
+	}
+}
---
+++ XML File/Assets/Scripts/ParseXMLInventory.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.IO;
+using TMPro;
+
+
+public class ParseXMLInventory : MonoBehaviour
+{   public TextAsset scoreDataTextFile;
+    public TextMeshProUGUI textDisplay;
+
+    void Start()
+    {
+        string textData = scoreDataTextFile.text;
+        ParseScoreXML(textData);
+    }
+
+    void ParseScoreXML(string xmlData)
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(new StringReader(xmlData));
+
+        string xmlPathPattern = "//inventory/food";
+        XmlNodeList myNodeList = xmlDoc.SelectNodes(xmlPathPattern);
+
+        foreach (XmlNode node in myNodeList)
+            textDisplay.text += ScoreRecordString(node) + "<br>";
+        //Debug.Log(ScoreRecordString(node));
+
+        XmlNodeList item = xmlDoc.GetElementsByTagName("cost");
+        textDisplay.text += item[0].Name + ": " + item[0].ChildNodes[0].InnerText;
+    }
+
+    string ScoreRecordString(XmlNode node)
+    {
+        XmlNode playerNode = node.FirstChild;
+        XmlNode scoreNode = playerNode.NextSibling;
+
+       
+
+        return "cost = " + playerNode.InnerXml + ", quantity = " + scoreNode.InnerXml;
+    }
+
+    string DateString(XmlNode dateNode)
+    {
+        XmlNode dayNode = dateNode.FirstChild;
+        XmlNode monthNode = dayNode.NextSibling;
+        XmlNode yearNode = monthNode.NextSibling;
+
+        return dayNode.InnerXml + "/" + monthNode.InnerXml + "/" + yearNode.InnerXml;
+    }
+   
+}
