032fcaf12029aa20f09e94e13ebd4ae0e0390e72
Toastyship <64509653+Toastyship@users.noreply.github.com>
2022-01-06 10:24:18 +0800

    test
    test

--- FYPGame3/Assets/GleyPlugins/TrafficSystem/Scripts/ToUse/TrafficComponent.cs
+++ FYPGame3/Assets/GleyPlugins/TrafficSystem/Scripts/ToUse/TrafficComponent.cs
@@ -18,9 +18,9 @@ namespace GleyTrafficSystem
         [Tooltip("Distance from the player where a vehicle can be removed")]
         public float distanceToRemove = 150;
         [Tooltip("How long yellow light is on (if = -1 the value from the intersection component will be used)")]
-        public float yellowLightTime = 3;
+        public float yellowLightTime =1;
         [Tooltip("How long green light is on (if = -1 the value from the intersection component will be used)")]
-        public float greenLightTime = 5;
+        public float greenLightTime = 1;
 
         void Start()
         {
