5f2b2e924aab4dd07128375a7e48d100e01043ac
WanWeiJie <64512487+WanWeiJie@users.noreply.github.com>
2022-01-06 14:09:27 +0800

    Passenger

---
+++ FYPGame3/Assets/Scripts/CarScripts/CarMasterControl/Earnings.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Earnings : MonoBehaviour
+{
+    public Text earningText;
+    public Text endOfDayText;
+
+    int earnings = 0;
+
+    public static Earnings instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        earningText.text = " : " + earnings.ToString();
+        endOfDayText.text = " Earnings for today: " + earnings.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void EarnMoney()
+    {
+        earnings += 300;
+        earningText.text = " : " + earnings.ToString();
+        endOfDayText.text = " Earnings for today: " + earnings.ToString();
+    }
+}
---
+++ FYPGame3/Assets/Scripts/CarScripts/CarMasterControl/TaxiManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TaxiManager : MonoBehaviour
+{
+    public GameObject passenger;
+    public bool passengerInTaxi;
+    public GameObject destination;
+    public bool destinationReached;
+    public GameObject reachedPassenger;
+    public GameObject summary;
+    //public GameObject arrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        passenger = GameObject.FindGameObjectWithTag("Passenger");
+        destination = GameObject.FindGameObjectWithTag("Destination");
+        passengerInTaxi = false;
+        destinationReached = false;
+        reachedPassenger.SetActive(false);
+        summary.SetActive(false);
+        //arrow.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (passengerInTaxi)
+        {
+            //passenger.transform.SetParent(this.transform);
+            passenger.SetActive(false);
+            //Destroy(passenger);
+            Debug.Log("Gone");
+        }
+
+        if (destinationReached)
+        {
+            reachedPassenger.SetActive(true);
+            StartCoroutine(EndDay());
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Passenger")
+        {
+            passengerInTaxi = true;
+            //arrow.SetActive(true);
+        }
+        else if (other.gameObject.tag == "Destination")
+        {
+            destinationReached = true;
+
+            Earnings.instance.EarnMoney();
+            //arrow.SetActive(true);
+        }
+    }
+
+    IEnumerator EndDay()
+    {
+        yield return new WaitForSeconds(5);
+        summary.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
