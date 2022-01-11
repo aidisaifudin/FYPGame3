a87fbf3b7a7b28feb528e9e456ae307dfaa0b6df
Toastyship <64509653+Toastyship@users.noreply.github.com>
2022-01-11 16:20:16 +0800

    S
    S

---
+++ FYPGame3/Assets/Scripts/DialogueScript/DialogueScript2.0/NPC.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogue", menuName = "NPC Dialogue")]
+public class NPC : ScriptableObject {
+	public new string name;
+	[TextArea(3, 15)]
+	public string[] dialogue;
+	[TextArea(3, 15)]
+	public string[] playerDialogue;
+}
