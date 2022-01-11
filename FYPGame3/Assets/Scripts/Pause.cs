f8b5f44035ac06f370a39b4e98b22e45000f40cf
Aidi <aidi.saifudin@gmail.com>
2022-01-11 15:31:32 +0800

    pause panel done

--- FYPGame3/Assets/Scripts/Pause.cs
+++ FYPGame3/Assets/Scripts/Pause.cs
@@ -7,14 +7,23 @@ using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
+    public GameObject pausePanel;
+
+    public void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
     public void PauseGame()
     {
         Time.timeScale = 0;
+        pausePanel.SetActive(true);
     }
 
     public void ResumeGame()
     {
         Time.timeScale = 1;
+        pausePanel.SetActive(false);
     }
 
     public void GoMainMenu()
