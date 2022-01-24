48d3fa141d624d6a7d1cd498b739a40c8417ba66
Aidi <aidi.saifudin@gmail.com>
2022-01-24 14:40:18 +0800

    credit scene done

--- FYPGame3/Assets/Scripts/FadeEffects.cs
+++ FYPGame3/Assets/Scripts/FadeEffects.cs
@@ -7,42 +7,31 @@ public class FadeEffects : MonoBehaviour
 {
     public TMP_Text textCredits;
 
-    public float fadeSpeed = 5.0f;
-    public bool entrance;
-    public GameObject canvas;
+    
+    
+    public Animator animator;
 
     public void Start()
     {
         textCredits = FindObjectOfType<TMP_Text>();
+      
     }
 
     void Update()
     {
-        ColorChange();
+       
     }
 
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Player")
         {
-            entrance = true;
+           
+            animator.SetBool("FadeIn", true);
+            Debug.Log("hit");
+
         }
     }
 
-   
-
-    void ColorChange()
-    {
-        if (entrance)
-        {
-            textCredits.color = Color.Lerp(new Color(1f, 1f, 1f, 0f), new Color(1f, 1f, 1f, 1f), fadeSpeed * Time.deltaTime);
- 
-        }
-
-        //if (!entrance)
-        //{
-        //    textCredits.color = Color.Lerp(textCredits.color, Color.clear, fadeSpeed * Time.deltaTime);
-            
-        //}
-    }
+    
 }
