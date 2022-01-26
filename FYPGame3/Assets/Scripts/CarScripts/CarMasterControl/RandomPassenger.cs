e9e8f9923e157f4b7ca0b964524079282839e9dc
WanWeiJie <64512487+WanWeiJie@users.noreply.github.com>
2022-01-26 15:30:00 +0800

    Passenger stuff

--- FYPGame3/Assets/Scripts/CarScripts/CarMasterControl/RandomPassenger.cs
+++ FYPGame3/Assets/Scripts/CarScripts/CarMasterControl/RandomPassenger.cs
@@ -8,6 +8,7 @@ public class RandomPassenger : MonoBehaviour
     public Vector3[] destinationSpawn;
     public GameObject passenger;
     public GameObject destination;
+    public Vector3[] nextPassenger;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +25,7 @@ public class RandomPassenger : MonoBehaviour
     {
         if (TaxiManager.destinationReached)
         {
+            //nextPassenger = Mathf.Abs(int.Parse(destinationSpawn[0].ToString()) - 1);
             int randomPassenger = Random.Range(0, passengerSpawn.Length);
             int randomDestination = Random.Range(0, destinationSpawn.Length);
 
@@ -31,5 +33,26 @@ public class RandomPassenger : MonoBehaviour
             Instantiate(destination, destinationSpawn[randomDestination], transform.rotation);
             TaxiManager.destinationReached = false;
         }
+    }
+
+    Vector3[] ChooseRandomDestination(int numRequired)
+    {
+        Vector3[] result = new Vector3[numRequired];
+        int numToChoose = numRequired;
+
+        for (int numLeft = destinationSpawn.Length; numLeft > 0; numLeft--)
+        {
+            float probability = (numToChoose + 0.0f) / (numLeft + 0.0f); // Adding 0.0 is simply to cast the integers to float for division operation
+
+            if (Random.value <= probability)
+            {
+                numToChoose--;
+                result[numToChoose] = destinationSpawn[numLeft - 1];
+
+                if (numToChoose == 0)
+                    break;
+            }
+        }
+        return destinationSpawn;
     }
 }
