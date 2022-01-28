6cf4b6e06aaf3447fd822aaf6a4ccd752e947039
WanWeiJie <64512487+WanWeiJie@users.noreply.github.com>
2022-01-28 12:11:45 +0800

    a

---
+++ FYPGame3/Assets/Scripts/CarScripts/CarMasterControl/TutorialCar.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialCar : MonoBehaviour
+{
+    public GameObject passenger;
+    public GameObject destination;
+    public GameObject summary;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        passenger = GameObject.FindGameObjectWithTag("Passenger");
+        destination = GameObject.FindGameObjectWithTag("Destination");
+        summary.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        summary.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Passenger")
+        {
+            //passengerInTaxi = true;
+            Destroy(other.gameObject);
+            //arrow.SetActive(true);
+        }
+        else if (other.gameObject.tag == "Destination")
+        {
+            //destinationReached = true;
+
+            Earnings.instance.EarnMoney();
+            Destroy(other.gameObject);
+            //RandomPassenger.instance.SpawnPassenger();
+            //arrow.SetActive(true);
+        }
+    }
+}
