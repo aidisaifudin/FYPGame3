3022764b93de693137ec84f926b5b5783889dc69
Toastyship <64509653+Toastyship@users.noreply.github.com>
2022-02-03 09:29:22 +0800

    S
    S

--- FYPGame3/Assets/Scripts/CarScripts/CarMasterControl/RandomPassenger.cs
+++ FYPGame3/Assets/Scripts/CarScripts/CarMasterControl/RandomPassenger.cs
@@ -20,7 +20,7 @@ public class RandomPassenger : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         int randomPassenger = Random.Range(0, passengerSpawn.Length);
         //int pickedPassenger = closePickupPoint[randomPassenger][4];
