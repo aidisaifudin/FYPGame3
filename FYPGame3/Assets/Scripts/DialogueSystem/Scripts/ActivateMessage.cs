ff2e65e8872165a352cf1c245c6b886bba02a912
Aidi <aidi.saifudin@gmail.com>
2022-02-09 20:03:03 +0800

    accident scene done

---
+++ FYPGame3/Assets/Scripts/DialogueSystem/Scripts/ActivateMessage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateMessage : MonoBehaviour {
+	public GameObject message;
+
+	public void ShowMessage() {
+		message.SetActive(true);
+	}
+}
