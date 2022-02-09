a42f9cce071fd24ec511e0c7903f60ee00ce1918
Aidi <aidi.saifudin@gmail.com>
2022-02-10 00:11:34 +0800

    tutorial done

--- FYPGame3/Assets/Scripts/TutorialScripts/TriggerTutorial.cs
+++ FYPGame3/Assets/Scripts/TutorialScripts/TriggerTutorial.cs
@@ -6,16 +6,28 @@ using UnityEngine.SceneManagement;
 public class TriggerTutorial : MonoBehaviour
 {
     public GameObject panel;
+    public GameObject boxCollider;
+    public bool triggerHit;
+
+    public void Start()
+    {
+        triggerHit = false;
+    }
 
     public void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+            triggerHit = true;
             panel.SetActive(true);
+            Destroy(boxCollider);
             Time.timeScale = 0;
+           
         }
+        
     }
 
+    
     public void EndTutorial()
     {
         SceneManager.LoadScene("MainGame");
