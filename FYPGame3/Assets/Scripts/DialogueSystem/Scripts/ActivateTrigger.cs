cae1d7a700ef1b85878fa13165e1c75cc709e673
Aidi <aidi.saifudin@gmail.com>
2022-03-02 11:17:03 +0800

    dialogue

--- FYPGame3/Assets/Scripts/DialogueSystem/Scripts/ActivateTrigger.cs
+++ FYPGame3/Assets/Scripts/DialogueSystem/Scripts/ActivateTrigger.cs
@@ -29,6 +29,36 @@ public class ActivateTrigger : MonoBehaviour {
                 Destroy(other.gameObject);
                 Time.timeScale = 0;
                 break;
+            case "Passenger4":
+                index = 3;
+                Destroy(other.gameObject);
+                Time.timeScale = 0;
+                break;
+            case "Passenger5":
+                index = 4;
+                Destroy(other.gameObject);
+                Time.timeScale = 0;
+                break;
+            case "Passenger6":
+                index = 5;
+                Destroy(other.gameObject);
+                Time.timeScale = 0;
+                break;
+            case "Passenger7":
+                index = 6;
+                Destroy(other.gameObject);
+                Time.timeScale = 0;
+                break;
+            case "Passenger8":
+                index = 7;
+                Destroy(other.gameObject);
+                Time.timeScale = 0;
+                break;
+            case "Passenger9":
+                index = 8;
+                Destroy(other.gameObject);
+                Time.timeScale = 0;
+                break;
         }
 	}
 }
