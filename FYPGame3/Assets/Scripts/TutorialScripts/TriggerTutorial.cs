c992c15e25bac0fc2257b788243a7eb165bff41b
Aidi <aidi.saifudin@gmail.com>
2022-03-10 18:33:45 +0800

    TUTORIAL BAHASA DONE

--- FYPGame3/Assets/Scripts/TutorialScripts/TriggerTutorial.cs
+++ FYPGame3/Assets/Scripts/TutorialScripts/TriggerTutorial.cs
@@ -6,17 +6,10 @@ using TMPro;
 
 public class TriggerTutorial : MonoBehaviour
 {
-    public GameObject panel;
+    public GameObject panelEnglish;
+    public GameObject panelBahasa;
     public GameObject boxCollider;
-    public TMP_Text message1;
-    public TMP_Text message2;
-    public TMP_Text message3;
-    public TMP_Text message4;
-    public TMP_Text message5;
-    public TMP_Text message6;
-    public TMP_Text message7;
-    public TMP_Text message8;
-    public TMP_Text message9;
+   
     public bool triggerHit;
 
     public void Start()
@@ -30,38 +23,27 @@ public class TriggerTutorial : MonoBehaviour
         {
 
             triggerHit = true;
-            panel.SetActive(true);
+            
             Destroy(boxCollider);
             Time.timeScale = 0;
 
             switch (SetLanguage.languageIndex)
             {
                 case 0: // Bahasa
-                    message1.text = "Selamat datang ngentot";
-                    message2.text = "Dont die man";
-                    message3.text = "TAP PADA KOTAK DIALOG UNTUK GULIR";
-                    message4.text = "TAP PADA KOTAK DIALOG UNTUK GULIR";
-                    message5.text = "TAP PADA KOTAK DIALOG UNTUK GULIR";
-                    message6.text = "TAP PADA KOTAK DIALOG UNTUK GULIR";
-                    message7.text = "TAP PADA KOTAK DIALOG UNTUK GULIR";
-                    message8.text = "TAP PADA KOTAK DIALOG UNTUK GULIR";
-                    message9.text = "TAP PADA KOTAK DIALOG UNTUK GULIR";
+                    
+                    panelBahasa.SetActive(true);
+               
                     break;
                 case 1: // English
-                    message1.text = "Hi welcome back bitch";
-                    message2.text = "Mampus lu anjing";
-                    message3.text = "TAP PADA KOTAK DIALOG UNTUK GULIR";
-                    message4.text = "TAP PADA KOTAK DIALOG UNTUK GULIR";
-                    message5.text = "TAP PADA KOTAK DIALOG UNTUK GULIR";
-                    message6.text = "TAP PADA KOTAK DIALOG UNTUK GULIR";
-                    message7.text = "TAP PADA KOTAK DIALOG UNTUK GULIR";
-                    message8.text = "TAP PADA KOTAK DIALOG UNTUK GULIR";
-                    message9.text = "TAP PADA KOTAK DIALOG UNTUK GULIR";
+                    
+                    panelEnglish.SetActive(true);
                     break;
             }
 
         }
+
         
+
     }
 
     
